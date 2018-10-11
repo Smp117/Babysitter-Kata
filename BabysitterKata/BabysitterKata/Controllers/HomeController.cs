aaf5dd61c77cca9dab59c6858af8e13c4723cb4d
Sean Peoples <smp117@zips.uakron.edu>
2018-10-11 16:27:19 -0400

    Added basic view and javascript.  Also modified the controller logic to react more intuitively

--- BabysitterKata/BabysitterKata/Controllers/HomeController.cs
+++ BabysitterKata/BabysitterKata/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using BabysitterKata.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services;
 
 namespace BabysitterKata.Controllers
 {
@@ -11,11 +13,8 @@ namespace BabysitterKata.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
+            int start = 0, bed = 0, end = 0;
 
-        public ActionResult UpdateDropDowns(int start, int bed, int end)
-        {
             var times = Time.AvailableTimes();
             HoursViewModel viewModel = new HoursViewModel();
             // Start time can be any available time
@@ -29,9 +28,33 @@ namespace BabysitterKata.Controllers
             // Filter dropdowns to only display appropriate hours
             viewModel.StartOptions = times;
             viewModel.BedTimeOptions = times.Where(x => x.Value >= viewModel.BedTimeHour.Value).ToList();
-            viewModel.BedTimeOptions = times.Where(x => x.Value >= viewModel.EndHour.Value).ToList();
-            
-            return PartialView(viewModel);
+            viewModel.EndOptions = times.Where(x => x.Value >= viewModel.EndHour.Value).ToList();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public string UpdateDropDowns(int start, int bed, int end)
+        {
+            var times = Time.AvailableTimes();
+            HoursViewModel viewModel = new HoursViewModel();
+            // Start time can be any available time
+            viewModel.StartHour = times.First(x => x.Value == start);
+            // Bed can't be any earlier than the start time
+            viewModel.BedTimeHour = bed > start ? times.First(x => x.Value == bed) : times.First(x => x.Value == start);
+            // End time can't be before bed time and has to be after start time
+            // If the kid won't be in bed before the job is finished, bedtime and end time will be the same
+            viewModel.EndHour = end > bed ? times.First(x => x.Value == end) : bed > start ? times.First(x => x.Value == bed) : times.First(x => x.Value == bed + 1);
+
+            // Filter dropdowns to only display appropriate hours
+            viewModel.StartOptions = Time.AvailableTimes();
+            viewModel.StartOptions.RemoveAt(viewModel.StartOptions.Count() - 1);
+            viewModel.BedTimeOptions = times.Where(x => x.Value >= viewModel.StartHour.Value).ToList();
+            if(start >= end)
+                viewModel.EndOptions = times.Where(x => x.Value >= viewModel.BedTimeHour.Value + 1).ToList();
+            else
+                viewModel.EndOptions = times.Where(x => x.Value >= viewModel.BedTimeHour.Value).ToList();
+
+            return JsonConvert.SerializeObject(viewModel);
         }
     }
 }
---
+++ BabysitterKata/BabysitterKata/Scripts/Pages/Index.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    $('.hourddl').on("change", function () {
+        console.log("change");
+        UpdateDropdowns();
+    });
+
+    function UpdateDropdowns() {
+        var start = $('#StartHour').val();
+        var bed = $('#BedTimeHour').val();
+        var end = $('#EndHour').val();
+
+        //console.log(start);
+        //console.log(bed);
+        //console.log(end);
+
+        $.ajax({
+            method: "POST",
+            url: "Home/UpdateDropDowns",
+            data: { start: start, bed: bed, end: end}
+        })
+            .done(function (data) {
+                var result = JSON.parse(data);
+                UpdateDropdown($('#StartHour'), result.StartOptions, start);
+                UpdateDropdown($('#BedTimeHour'), result.BedTimeOptions, bed);
+                UpdateDropdown($('#EndHour'), result.EndOptions, end);
+            });
+    }
+
+    function UpdateDropdown($selector, list, selectedValue) {
+        $selector.empty();
+        $.each(list, function (index, item) {
+            $selector.append(
+                $('<option>', {
+                    value: item.Value,
+                    text: item.Display
+                }, '</option>'))
+            $selector.val(selectedValue);
+            if ($selector.val() == null) 
+                $selector.prop("selectedIndex", 0);
+        })
+    }
+
+});
