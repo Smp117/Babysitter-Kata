91f2825e7ea5b3d91ac38590d9018b64eeb43e2c
Sean Peoples <smp117@zips.uakron.edu>
2018-10-10 21:48:11 -0400

    Created First Round of Models - wip

---
+++ BabysitterKata/BabysitterKata/Models/HoursViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BabysitterKata.Models
+{
+    public class HoursViewModel
+    {
+        public Time StartHour { get; set; }
+        public Time BedTimeHour { get; set; }
+        public Time EndHour { get; set; }
+    }
+}
---
+++ BabysitterKata/BabysitterKata/Models/Time.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BabysitterKata.Models
+{
+    public class Time
+    {
+        public string Display { get; set; }
+        public int Value { get; set; }
+
+        public Time(string display, int value)
+        {
+            Display = display;
+            Value = value;
+        }
+
+        public static List<Time> AvailableTimes()
+        {
+            return new List<Time>()
+            {
+                new Time("5:00 PM", 0),
+                new Time("6:00 PM", 1),
+                new Time("7:00 PM", 2),
+                new Time("8:00 PM", 3),
+                new Time("9:00 PM", 4),
+                new Time("10:00 PM", 5),
+                new Time("11:00 PM", 6),
+                new Time("12:00 AM", 7),
+                new Time("1:00 AM", 8),
+                new Time("2:00 AM", 9),
+                new Time("3:00 AM", 10),
+                new Time("4:00 AM", 11)
+            };
+        }
+    }
+}
