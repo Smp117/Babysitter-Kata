6f0a6e7912a199d84233a9a0b3211e377ad2116c
Sean Peoples <smp117@zips.uakron.edu>
2018-10-14 17:17:34 -0400

    Moved the logic to create a viewmodel from the controller to a constructor and modified the dropdown value logic

--- BabysitterKata/BabysitterKata/Models/HoursViewModel.cs
+++ BabysitterKata/BabysitterKata/Models/HoursViewModel.cs
@@ -8,6 +8,34 @@ namespace BabysitterKata.Models
 {
     public class HoursViewModel
     {
+        public HoursViewModel(int start, int bed, int end)
+        {
+            var times = Time.AvailableTimes();
+            // Start time can be any available time
+            StartHour = times.First(x => x.Value == start);
+            // Bed can't be any earlier than the start time
+            BedTimeHour = bed > start ? times.First(x => x.Value == bed) : times.First(x => x.Value == start);
+            // End time can't be before bed time and has to be after start time
+            // If the kid won't be in bed before the job is finished, bedtime and end time will be the same
+            if (end > BedTimeHour.Value)
+                EndHour = times.First(x => x.Value == end);
+            else if (BedTimeHour.Value > StartHour.Value)
+                EndHour = times.First(x => x.Value == BedTimeHour.Value);
+            else
+                EndHour = times.First(x => x.Value == BedTimeHour.Value + 1);
+
+            //EndHour = end > bed ? times.First(x => x.Value == end) : bed > start ? times.First(x => x.Value == bed) : times.First(x => x.Value == bed + 1);
+
+            // Filter dropdowns to only display appropriate hours
+            StartOptions = Time.AvailableTimes();
+            StartOptions.RemoveAt(StartOptions.Count() - 1);
+            BedTimeOptions = times.Where(x => x.Value >= StartHour.Value).ToList();
+            if (start >= end || start >= bed)
+                EndOptions = times.Where(x => x.Value >= BedTimeHour.Value + 1).ToList();
+            else
+                EndOptions = times.Where(x => x.Value >= BedTimeHour.Value).ToList();
+        }
+
         private static int PreBedRate { get { return 12; } }
         private static int PostBedRate { get { return 8; } }
         private static int PostMidnightRate { get { return 16; } }
@@ -26,8 +54,7 @@ namespace BabysitterKata.Models
             // If Start time is after midnight, there is only 1 rate to care about
             if (StartHour.Value >= Time.Midnight.Value)
                 return (EndHour.Value - StartHour.Value) * PostMidnightRate;
-
-
+            
             int start = 0;
             int postBed = 0;
             int postMidnight = 0;
